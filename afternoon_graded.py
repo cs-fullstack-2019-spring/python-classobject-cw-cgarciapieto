8ce549814d589a6dd277b2c01ffc6a392eb46899
carlos <cgarciapieto@gmail.com>
2019-01-25 15:56:36 -0600

    grade afternoon

---
+++ afternoon_graded.py
@@ -0,0 +1,40 @@
+#python code that creates a class of objects then prints it out based on user preference
+
+
+class Dog:
+    def __init__(self, name="Bobby", breed="Lab", color="red", gender="female"):
+        self.name = name
+        self.breed = breed
+        self.color = color
+        self.gender = gender
+
+    def printAll(self):
+        print(self.name, self.breed, self.color, self.gender)
+
+def main():
+        problem2()
+
+
+
+def problem2():
+        userInput = ""
+        while(userInput != "="):
+            userInput = input("Enter the right sign *hint*(equal)")
+
+def problem3():
+
+    threePersons = 
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
